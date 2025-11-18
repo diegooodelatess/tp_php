97fcffa9116c1f798464070414cadb0a207bc6eb
diego h <diego_hekimian@icloud.com>
2025-11-18 12:52:54 +0100

    nouveau fichier

---
+++ moyenne_tab.php
@@ -0,0 +1,14 @@
+<?php
+$eleves = [
+  ["nom" => "Alice", "notes" => [15, 14.5, 16]],
+  ["nom" => "Bob", "notes" => [12, 10, 11]],
+  ["nom" => "Claire", "notes" => [18, 17, 16]]
+];
+
+foreach ($eleves as $eleve) {
+  $nom = $eleve["nom"];
+  $notes = $eleve["notes"];
+  $moyenne = array_sum($notes) / count($notes);
+  echo $nom . number_format($moyenne,2) . "\n";
+}
+?>
